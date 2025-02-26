fe62dd5845c6712f739a069ab3dd30de35938ae2
DianYukh <dianayukhymukua@gmail.com>
2025-02-26 20:08:32 +0100

    add OrderController, OrderService

--- src/main/java/com/example/vendingmachinespring/controller/OrderController.java
+++ src/main/java/com/example/vendingmachinespring/controller/OrderController.java
@@ -1,5 +1,43 @@
 package com.example.vendingmachinespring.controller;
 
+import com.example.vendingmachinespring.model.Order;
+import com.example.vendingmachinespring.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+    private OrderService orderService;
+    @Autowired
+    public void setOrderService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Order> getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PostMapping("/{productId}/{quantity}")
+    public Order createOrder(@PathVariable Long productId, @PathVariable int quantity) {
+        return orderService.createOrder(productId, quantity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+
+
 
 }
--- src/main/java/com/example/vendingmachinespring/service/OrderService.java
+++ src/main/java/com/example/vendingmachinespring/service/OrderService.java
@@ -1,5 +1,54 @@
 package com.example.vendingmachinespring.service;
 
+import com.example.vendingmachinespring.model.Order;
+import com.example.vendingmachinespring.model.Product;
+import com.example.vendingmachinespring.repository.OrderRepository;
+import com.example.vendingmachinespring.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+    private OrderRepository orderRepository;
+    private ProductRepository productRepository;
+    @Autowired
+    public OrderService(OrderRepository orderRepository, ProductRepository productRepository) {
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+    }
+
+    public Order findById(Long idOrder) {
+        return orderRepository.findById(idOrder).get();
+    }
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order createOrder(Long productId, int quantity) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product is not found"));
+
+        if (product.getQuantity() < quantity) {
+            throw new RuntimeException("Not enough goods in stock");
+        }
+
+        double totalPrice = product.getPrice() * quantity;
+        Order order = new Order(product, quantity, totalPrice);
+
+        product.setQuantity(product.getQuantity() - quantity);
+        productRepository.save(product);
+
+        return orderRepository.save(order);
+    }
+
+    public void deleteOrder(Long idOrder) {
+        orderRepository.deleteById(idOrder);
+    }
 
 }
